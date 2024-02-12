1f8e1cf040986df5ee93b5afa16ecb483378ae80
Furkan Kümüş <furkankumus1905@gmail.com>
2024-02-12 16:38:58 +0300

    a finished project
    update v1.0

---
+++ BorsaUygulama/BorsaUygulamaApi/EF/BorsaAppContext.cs
@@ -0,0 +1,21 @@
+﻿using BorsaUygulamaApi.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+
+namespace BorsaUygulamaApi.EF
+{
+    public class BorsaAppContext : DbContext
+    {
+        public DbSet<Uyelik> Uyelik { get; set; }
+        public DbSet<Giris> Giris { get; set; }
+        public DbSet<HisseAdlari> HisseAd { get; set; }
+        public DbSet<HisseBilgileriKayit> HisseBilgiKayit { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=.\\SQLEXPRESS; Database=BorsaDB; User Id=zlatan;Password=123456; TrustServerCertificate=true;Integrated Security=SSPI");
+
+            base.OnConfiguring(optionsBuilder);
+        }
+    }
+}
